32ebd226432a385bb1cd20da76760bceb02879be
Mrlishizhen <718647063@qq.com>
2023-02-25 15:17:35 +0800

    添加echrts

--- src/views/component/echarts/index.tsx
+++ src/views/component/echarts/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.less'
 const Echarts = ()=>{
     return(
-        <div className={styles.echart_box}></div>
+        <div className={styles.echarts_box}></div>
     )
 }
 export default Echarts
